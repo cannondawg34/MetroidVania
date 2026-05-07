39b4d5001741ac439d16e292d3cbcbcfe43d567e
cannondawg34 <103911101+cannondawg34@users.noreply.github.com>
2026-05-06 20:16:02 -0400

    Worked on player movement, added mcp to my enviorment, updated git ignore so that Marbu and I don't have any merge conflicts.

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -7,21 +7,26 @@ public class Player_Movement : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float j;
     private Rigidbody2D body;
-    private Animator anim;
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
+        //get input and then applies it to rb 
         float horizontalInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+
+
     }
         // Start is called before the first frame update
         void Start()
     {
         
     }
+
+
+
 }
