e84ff4c4d5de451b925e8ea8ed0d733904ee1d26
cannondawg34 <103911101+cannondawg34@users.noreply.github.com>
2026-05-17 14:12:36 -0400

    Worked briefly on jump mechanic

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Player_Movement : MonoBehaviour
 {
     [SerializeField] private float speed;
-    [SerializeField] private float j;
+    [SerializeField] private float j = 5;
+    bool grounded = false;
     private Rigidbody2D body;
     private void Awake()
     {
@@ -20,13 +21,29 @@ public class Player_Movement : MonoBehaviour
 
 
 
+        if (Input.GetKeyDown(KeyCode.J)) //&& notGrounded
+        {
+            Jump();
+        }
     }
-        // Start is called before the first frame update
-        void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        
+
     }
 
+    private void Jump()
+    {
+        //anim SetANIM
+        body.velocity = new Vector2(body.velocity.x, j);
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
 
+        }
+    }
 }
